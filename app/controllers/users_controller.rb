7c669abb7509121bedd1f9cb8953a4dd249d9a4f
Anri Liy <anri@realquantum.com>
2019-10-30 06:44:10 -0500

    User registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'User is successfully signed up'
+      redirect_to @user  
+    else  
+      flash.now[:danger] = "User is not successfully sigend up"
+      render :new
+    end
+  end
+
+  private 
+
+  def user_params 
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
