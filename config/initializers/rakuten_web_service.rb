28d0dfe878a9decc35524a190c5c89d3882cb426
Anri Liy <anri@realquantum.com>
2019-11-30 10:41:01 -0600

    Feature API

--- config/initializers/rakuten_web_service.rb
+++ config/initializers/rakuten_web_service.rb
@@ -1,6 +1,6 @@
 RakutenWebService.configure do |c|
   # (Required) Appliction ID for your application.
-  c.application_id = 'YOUR_APPLICATION_ID'
+  c.application_id = ENV['APPLICATION_ID']
 
   # (Optional) Affiliate ID for your Rakuten account.
   c.affiliate_id = 'YOUR_AFFILIATE_ID' # default: nil
